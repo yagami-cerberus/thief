b44fce7edc536d0c11415bb3633d976278bc58d1
Cerberus <cerberus1220@gmail.com>
2014-08-04 15:53:12 +0800

    Create prepare product page, puts all dummy tasks in it like get image, get jan..

--- thief/products/views.py
+++ thief/products/views.py
@@ -15,6 +15,7 @@ from thief.products.models import Product, ProductImage
 from thief.products import forms
 from thief.auction.csv_packer import CsvPacker, CsvUnpacker
 from thief.auction.models import Keyword
+from thief.vendors.rakuten import Rakuten
 from thief.vendors.models import Cache
 from thief.vendors import ProductOverview, GoogleImageSearch
 from thief.rest import ThiefREST
@@ -26,9 +27,11 @@ SESSION_LAST_UPLOAD_PRODUCTS_KEY = 'lupk'
 class products(ThiefREST):
     template = 'products/products.html'
     
+    # List
     def get(self, request):
         return {'products': Product.objects.order_by('title').all()}
-        
+    
+    # Create
     def post(self, request):
         v_product = ProductOverview.from_dict({key: request.POST[key] for key in request.POST})
         product = Product(vendor=v_product.vendor, item_id=v_product.item_id,
@@ -38,10 +41,11 @@ class products(ThiefREST):
             size=v_product.size)
         product.save()
         
-        self.auto_fetch_image(product)
-        
-        return redirect(reverse('product', args=(product.id, )))
+        # self.try_fetch_image(product)
+        return redirect(reverse('prepare_product', args=(product.id, )))
+        # return redirect(reverse('product', args=(product.id, )))
     
+    # Delete
     def delete(self, request):
         for p in Product.objects.filter(pk__in=request.POST.getlist('p')):
             logger.info("Delete product: %s" % p)
@@ -55,7 +59,13 @@ class products(ThiefREST):
             
         return redirect(reverse('products'))
     
-    def auto_fetch_image(self, product):
+    # Special hook
+    def try_fetch_jan(self, productOverview):
+        if productOverview.url.startswith("http://product.rakuten.co.jp/"):
+            productOverview.jan = Rakuten().fetch_jan(productOverview.url)
+        
+    # Special hook
+    def try_fetch_image(self, product):
         gis = GoogleImageSearch()
         is_cache, gis_images = gis.search(product.title)
         if len(gis_images) > 0:
@@ -67,6 +77,39 @@ class product(ThiefREST):
     def get(self, request, id):
         product = Product.objects.get(id=id)
         return {'p': product}
+
+class prepare_product(product):
+    template = 'products/prepare_product.html'
+    
+    def post(self, request, id):
+        product = Product.objects.get(id=id)
+        action = request.POST.get("job")
+        
+        if action == "google-img":
+            return self.google_img(product)
+        elif action == "jan":
+            return self.jan(product)
+        else:
+            return HttpResponse("false", content_type="application/json")
+        
+    def google_img(self, product):
+        gis = GoogleImageSearch()
+        is_cache, gis_images = gis.search(product.title)
+        if len(gis_images) > 0:
+            product.fetch_image_from_url(gis_images[0].url)
+            return HttpResponse("true", content_type="application/json")
+        else:
+            return HttpResponse("false", content_type="application/json")
+
+    def jan(self, product):
+        if product.url.startswith("http://product.rakuten.co.jp/"):
+            jan = Rakuten().fetch_jan(product.url)
+            if jan:
+                product.jan = jan
+                product.save()
+                return HttpResponse("true", content_type="application/json")
+            else:
+                return HttpResponse("false", content_type="application/json")
     
 class edit_product(ThiefREST):
     template = 'products/edit.html'
--- thief/urls.py
+++ thief/urls.py
@@ -18,12 +18,13 @@ urlpatterns = patterns('',
     url(r'^vendor/search$', 'thief.vendors.views.search_product', name='search'),
     url(r'^vendor/query.(?P<format>(json))$', 'thief.vendors.views.query_vendor', name='query_vendor'),
 
-    url(r'^product$', 'thief.products.views.products', name='products'),
+    url(r'^products$', 'thief.products.views.products', name='products'),
     
     url(r'^products/download/(?P<auction_type>((yahoo)|(ruten))).zip$', 'thief.products.views.download_csv', name='download_products_csv'),
     url(r'^products/upload$', 'thief.products.views.upload_csv', name='upload_products_csv'),
 
     url(r'^product/(?P<id>\d+)$', 'thief.products.views.product', name='product'),
+    url(r'^product/(?P<id>\d+)/prepare$', 'thief.products.views.prepare_product', name='prepare_product'),
     url(r'^product/(?P<id>\d+)/edit$', 'thief.products.views.edit_product', name='edit_product'),
     url(r'^product/(?P<id>\d+)/edit_image$', 'thief.products.views.edit_image', name='edit_product_images'),
     
