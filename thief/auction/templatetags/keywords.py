cc9ba6d2008f49e20af3306ac6d39d57887c867f
Cerberus <cerberus1220@gmail.com>
2014-08-10 15:26:56 +0800

    Use template to build keyword group selector

---
+++ thief/auction/templatetags/keywords.py
@@ -0,0 +1,21 @@
+from django.utils.crypto import get_random_string
+from django.template.loader import get_template
+from django.template import Context
+from django import template
+
+from thief.auction.models import Keyword, KeywordSet
+
+register = template.Library()
+
+@register.simple_tag(name='load_keyword_groups', takes_context=True)
+def load_keyword_groups(context):
+    context['keyword_groups'] = Keyword.get_groups()
+    return ""
+    
+@register.simple_tag(name='keyword_group_dropdown_selector')
+def keyword_group_dropdown_selector(input_selector):
+    t = get_template("auction/__keyword_dropdown_selector.html")
+    return t.render(Context({
+        'keyword_groups': Keyword.get_groups(),
+        'input_selector': input_selector,
+        'identify': get_random_string(6)}))
