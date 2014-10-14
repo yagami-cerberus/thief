baa3db434f6718f834188ac47773317f4c5975ab
Cerberus <cerberus1220@gmail.com>
2014-10-14 23:10:15 +0800

    Add auto language translate for manufacturer

--- thief/vendors/views.py
+++ thief/vendors/views.py
@@ -91,6 +91,20 @@ class import_item(ThiefRestAPI):
         if g:
             return g.keywords
 
+    def manufacturer_convert(self, orig_input):
+        m = {
+            "\xe3\x83\xa4\xe3\x83\x9e\xe3\x83\x8f\xe6\xa0\xaa\xe5\xbc\x8f\xe4\xbc\x9a\xe7\xa4\xbe": "YAMAHA",
+            "\xe3\x83\xa4\xe3\x83\x9e\xe3\x83\x8f": "YAMAHA",
+            "\xe3\x83\x91\xe3\x83\x8a\xe3\x82\xbd\xe3\x83\x8b\xe3\x83\x83\xe3\x82\xaf": "Panasonic",
+            "\xe3\x82\xbd\xe3\x83\x8b\xe3\x83\xbc\xe6\xa0\xaa\xe5\xbc\x8f\xe4\xbc\x9a\xe7\xa4\xbe": "SONY",
+            "\xe3\x82\xbd\xe3\x83\x8b\xe3\x83\xbc": "SONY",
+            "\xe3\x82\xb7\xe3\x83\xa3\xe3\x83\xbc\xe3\x83\x97\xe6\xa0\xaa\xe5\xbc\x8f\xe4\xbc\x9a\xe7\xa4\xbe": "Sharp",
+            "\xe3\x82\xb7\xe3\x83\xa3\xe3\x83\xbc\xe3\x83\x97": "Sharp",
+            "\xe3\x82\xab\xe3\x82\xb7\xe3\x82\xaa\xe8\xa8\x88\xe7\xae\x97\xe6\xa9\x9f": "CASIO",
+            "\xe3\x82\xab\xe3\x82\xb7\xe3\x82\xaa": "CASIO"
+        }
+        return m.get(orig_input, orig_input)
+    
     def post(self, request):
         model_id = request.POST.get('model_id')
         group = request.POST.get('group', '')
@@ -120,17 +134,16 @@ class import_item(ThiefRestAPI):
         except IndexError:
             pass
         
-        print(amazon.manufacturer.__class__)
-        product = Product(manufacturer=amazon.manufacturer, model_id=model_id, group=group,
-            release_date=amazon.release_date or rakuten.release_date, weight=amazon.weight,
-            size=amazon.size, price=price, summary="", color="", details="",
+        product = Product(manufacturer=self.manufacturer_convert(amazon.manufacturer),
+            model_id=model_id, group=group, release_date=amazon.release_date or rakuten.release_date,
+            weight=amazon.weight, size=amazon.size, price=price, summary="", color="", details="",
             jan=jan, created_at=current_local_time_str())
         
         product.keywords = self.guess_keywords(product)
         product.save()
-        
-        if not rakuten == self._shallow_result: self.write_ref(product, rakuten)
-        if not rakuten == self._shallow_result: self.write_ref(product, amazon)
+
+        if not isinstance(rakuten, self.Shallow): self.write_ref(product, rakuten)
+        if not isinstance(amazon, self.Shallow): self.write_ref(product, amazon)
         self.fetch_image(product)
         
         return {'st': True, 'url': reverse('product', args=(product.id, ))}
